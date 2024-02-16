f546cedd94b169c6276534a56a58b0b8503a6500
leonardo <leoruizz14@outlook.com>
2024-02-16 16:47:13 -0300

    feat: :sparkles: Implement Shared preferences in favorite heroes list

--- lib/controllers/heroes_controller.dart
+++ lib/controllers/heroes_controller.dart
@@ -1,23 +1,40 @@
 import 'package:marvel_app/controllers/api_controller.dart';
-import 'package:marvel_app/domain/models/hero.dart';
+import 'package:marvel_app/domain/models/marvel_hero.dart';
+import 'package:marvel_app/utils/shared_prefs.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HeroesController {
   final ApiController apiController = ApiController();
-  final List<MarvelHero> favoriteHeroes = [];
+  final SharedPrefs prefs = SharedPrefs();
   final List<MarvelHero> homeHeroes = [];
+  final List<MarvelHero> favoriteHeroes = [];
 
   Future<List<MarvelHero>> loadHomeHeroes(MarvelHero hero) async {
-    final List<MarvelHero> fetchHeroes = await apiController.getHeroes();
+    final List<MarvelHero> fetchHeroes = await apiController
+        .getHeroes(); //TODO: Usar essa função quando for utilizar lazy load, pois assim a lista será aumentada ao carregar, e observaremos através do BlocBuilder, quando carregar mais (ao scrollar o maximo possivel para baixo) ele irá atualizar a tela automaticamente.
     homeHeroes.addAll(fetchHeroes);
     return homeHeroes;
   }
 
-  List<MarvelHero> addFavoriteHero(MarvelHero hero) {
-    favoriteHeroes.add(hero); //TODO: MUDAR PARA SET, POIS NÃO PODE TER DOIS HERÓIS IGUAIS.
+  Future<List<MarvelHero>> getFavoriteHeroes() async {
+    final heroes = await prefs.getMarvelHeroesFromDatabase();
+    return heroes;
+  }
+
+  Future<List<MarvelHero>> addFavoriteHero(MarvelHero hero) async {
+    // final SharedPreferences prefs = await SharedPreferences.getInstance();
+    // prefs.setString(hero.id.toString(), hero.id.toString());
+    prefs.addToDatabase(hero);
+    favoriteHeroes.add(
+      hero,
+    ); //TODO: chamar shared preferences aqui, adicionando todas as informações do herói ao shared_prefs.
     return favoriteHeroes;
   }
 
-  List<MarvelHero> removeFavoriteHero(MarvelHero hero) {
+  Future<List<MarvelHero>> removeFavoriteHero(MarvelHero hero) async {
+    // final SharedPreferences prefs = await SharedPreferences
+    //     .getInstance(); //TODO: melhorar esta função, talvez juntar com a de cima.
+    // prefs.remove(hero.id.toString());
     favoriteHeroes.remove(hero);
     return favoriteHeroes;
   }
