9ad7cfed3923201ae3d5dc2d6197417878f0f6f0
leonardo <leoruizz14@outlook.com>
2024-02-16 16:54:11 -0300

    refactor: :recycle: Refactor functions in favorite Bloc

--- lib/data/bloc/favorite_bloc/favorites_bloc.dart
+++ lib/data/bloc/favorite_bloc/favorites_bloc.dart
@@ -2,9 +2,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marvel_app/controllers/heroes_controller.dart';
 import 'package:marvel_app/data/bloc/favorite_bloc/favorite_list_events.dart';
 import 'package:marvel_app/data/bloc/favorite_bloc/favorite_list_states.dart';
+import 'package:marvel_app/utils/shared_prefs.dart';
 
 class FavoritesBloc extends Bloc<FavoriteListEvents, FavoriteListStates> {
-  HeroesController heroesController = HeroesController();
+  final HeroesController _heroesController = HeroesController();
+  final SharedPrefs prefs = SharedPrefs();
 
   FavoritesBloc() : super(FavoriteLoadState()) {
     on<FavoriteLoadList>(
@@ -12,28 +14,34 @@ class FavoritesBloc extends Bloc<FavoriteListEvents, FavoriteListStates> {
         emit(FavoriteLoadState());
         try {
           emit(FavoriteSuccessState(
-              favoriteHeros: heroesController.favoriteHeroes));
+              favoriteHeroes: _heroesController.favoriteHeroes));
         } catch (e) {
           emit(FavoriteErrorState(errorMessage: e.toString()));
         }
       },
     );
+    void removeHero(emit, event) async {
+      if (_heroesController.favoriteHeroes.contains(event.hero)) {
+        emit(FavoriteSuccessState(
+            favoriteHeroes:
+                await _heroesController.removeFavoriteHero(event.hero)));
+      }
+    }
+
+    void addHero(emit, event) async {
+      if (!_heroesController.favoriteHeroes.contains(event.hero)) {
+        emit(FavoriteSuccessState(
+            favoriteHeroes:
+                await _heroesController.addFavoriteHero(event.hero)));
+      }
+    }
 
     on<AddFavorite>(
-      (event, emit) {
-        if (!heroesController.favoriteHeroes.contains(event.hero)) {
-          emit(FavoriteSuccessState(
-              favoriteHeros: heroesController.addFavoriteHero(event.hero)));
-        }
-      },
+      (event, emit) => addHero(emit, event),
     );
+
     on<RemoveFavorite>(
-      (event, emit) {
-        if (heroesController.favoriteHeroes.contains(event.hero)) {
-          emit(FavoriteSuccessState(
-              favoriteHeros: heroesController.removeFavoriteHero(event.hero)));
-        }
-      },
+      (event, emit) => removeHero(emit, event),
     );
   }
 }
