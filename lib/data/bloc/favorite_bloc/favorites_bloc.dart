25d15cae76d3e293595d15aff5bbd43432adfbbf
leonardo <leoruizz14@outlook.com>
2024-02-18 08:23:19 -0300

    refactor: :recycle: Rewrite Favorite Bloc main function

--- lib/data/bloc/favorite_bloc/favorites_bloc.dart
+++ lib/data/bloc/favorite_bloc/favorites_bloc.dart
@@ -2,23 +2,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marvel_app/controllers/heroes_controller.dart';
 import 'package:marvel_app/data/bloc/favorite_bloc/favorite_list_events.dart';
 import 'package:marvel_app/data/bloc/favorite_bloc/favorite_list_states.dart';
+import 'package:marvel_app/domain/models/marvel_hero.dart';
 import 'package:marvel_app/utils/shared_prefs.dart';
 
 class FavoritesBloc extends Bloc<FavoriteListEvents, FavoriteListStates> {
   final HeroesController _repo = HeroesController();
 
+  void getHeroes(emit) async {
+    final List<MarvelHero> heroes = await _repo.getFavoriteHeroes();
+
+    emit(FavoriteLoadState());
+    try {
+      emit(FavoriteSuccessState(favoriteHeroes: heroes));
+    } catch (e) {
+      emit(FavoriteErrorState(errorMessage: e.toString()));
+    }
+  }
 
   FavoritesBloc() : super(FavoriteLoadState()) {
     on<FavoriteLoadList>(
-      (event, emit) {
-        emit(FavoriteLoadState());
-        try {
-          emit(FavoriteSuccessState(
-              favoriteHeroes: _heroesController.favoriteHeroes));
-        } catch (e) {
-          emit(FavoriteErrorState(errorMessage: e.toString()));
-        }
-      },
+      (event, emit) => getHeroes(emit),
     );
     void removeHero(emit, event) async {
       if (_repo.favoriteHeroes.contains(event.hero)) {
