f8f3e583c5fee79bc0b9ac84458cb3932cb21081
leonardo <leoruizz14@outlook.com>
2024-02-18 08:27:18 -0300

    refactor: :recycle: Change API controller for heroes controller

--- lib/data/bloc/home_bloc/home_list_bloc.dart
+++ lib/data/bloc/home_bloc/home_list_bloc.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:marvel_app/controllers/api_controller.dart';
+import 'package:marvel_app/controllers/heroes_controller.dart';
 import 'package:marvel_app/data/bloc/home_bloc/home_list_events.dart';
 import 'package:marvel_app/data/bloc/home_bloc/home_list_states.dart';
 
 class HomeListBloc extends Bloc<HomeListEvent, HomeListStates> {
-  final ApiController _apiController = ApiController();
+  final HeroesController _heroesController = HeroesController();
 
   HomeListBloc() : super(HomeLoadingState()) {
     on<HomeLoadHeroes>((event, emit) => getHeroes(emit));
@@ -13,7 +13,7 @@ class HomeListBloc extends Bloc<HomeListEvent, HomeListStates> {
   void getHeroes(emit) async {
     try {
       emit(HomeLoadingState());
-      final heroes = await _apiController.getHeroes();
+      final heroes = await _heroesController.loadAllHeroes();
       emit(HomeSuccessState(heroes: heroes));
     } catch (e) {
       emit(HomeErrorState(errorMessage: e.toString()));
