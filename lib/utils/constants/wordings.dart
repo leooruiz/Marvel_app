cdc63f7f5990803596e0185f1c31c5cf7f0089c3
leonardo <leoruizz14@outlook.com>
2024-02-28 14:48:12 -0300

    refactor: :speech_balloon: Add more strings to wordings class.

--- lib/utils/constants/wordings.dart
+++ lib/utils/constants/wordings.dart
@@ -2,4 +2,7 @@ class Wordings {
   static const title = 'MARVEL Heroes';
   static const heroes = 'Heroes';
   static const favorites = 'Favorites';
+  static const noDescription = 'No informations about the hero.';
+  static const settings = 'Settings';
+  static const theme = 'Theme';
 }
